09e5d60d6ff7dbc85996f7c074781cab26036929
AlexisAntomarioni <145984731+AlexisAntomarioni@users.noreply.github.com>
2023-09-26 12:11:04 -0300

    Add files via upload

---
+++ Practica.py
@@ -0,0 +1,10 @@
+
+# Este es un archivo de prueba, sin actividades =) .
+
+# A partir del próximo módulo: M03_variablesydatos vas a tener actividades para realizar.
+
+print('Hola Mundo!')
+a = 20
+while a > 0:
+    print(a)
+    a -= 1
